2b7af8f4f244093a1255490fa4e7db3f5333b6ae
Wadah Adlan <wadahadlan@gmail.com>
2017-09-15 20:50:18 -0400

    Syncing States like the Titanic

--- src/game/lobby.jsx
+++ src/game/lobby.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { values } from "lodash";
 
-const { firebase } = window;
+import Rebase from 're-base';
+var firebase = require('firebase');
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyAQNTztslAbIFaZ306nO1zAxBz90B-47tc",
+  authDomain: "mafia-f786c.firebaseapp.com",
+  databaseURL: "https://mafia-f786c.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "872061463737"
+});
+
+const base = Rebase.createClass(app.database());
+
+// const { firebase } = window;
 
 export default class Lobby extends React.Component {
 	constructor(props) {
@@ -10,25 +23,49 @@ export default class Lobby extends React.Component {
 			id: this.props.match.params.id,
 			players: {}
 		};
+		// this.fetchPlayers = this.fetchPlayers.bind(this);
+		// this.listenForUpdates = this.listenForUpdates.bind(this);
 	}
+  //
+	// componentDidMount() {
+	// 	this.fetchPlayers(this.props.match.params.id).then(
+	// 		this.listenForUpdates(this.state.id)
+	// 	);
+	// }
+  //
+	// listenForUpdates(gameToken) {
+	// 	const db = firebase
+	// 		.database()
+	// 		.ref("/gamesrooms" + gameToken)
+	// 		.child("players")
+	// 		.on("value", snapshot => {
+	// 			if (this.state.players !== snapshot.val().players) {
+	// 				this.setState({
+	// 					players: snapshot.val().players
+	// 				});
+	// 			}
+	// 		});
+	// }
+  //
+	// fetchPlayers(id) {
+	// 	return firebase
+	// 		.database()
+	// 		.ref("gamerooms/")
+	// 		.child(this.state.id)
+	// 		.once("value", snapshot => {
+	// 			this.setState({
+	// 				players: snapshot.val().players
+	// 			});
+	// 		});
+	// }
 
-	componentDidMount() {
-		this.fetchPlayers(this.props.match.params.id);
-	}
-
-	fetchPlayers(id) {
-
-		let players = firebase
-			.database()
-			.ref("gamerooms/")
-			.child(this.state.id)
-			.once("value", snapshot => {
-        debugger
-				this.setState({
-					players: snapshot.val().players
-				});
-			});
-	}
+  componentDidMount() {
+    base.syncState(`gamerooms/${this.state.id}/players`,{
+      context: this,
+      state: 'players',
+      asArray: true
+    });
+  }
 
 	render() {
 		let allPlayers = values(this.state.players).map(player => (
